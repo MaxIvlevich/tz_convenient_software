7f0b61d00456e71258614fceed22eac0943a38c5
MaxIvlev <maxivichmax@gmail.com>
2025-05-12 10:58:32 +0300

    initial commit

---
+++ src/main/java/max/iv/tz_convenient_software/TzConvenientSoftwareApplication.java
@@ -0,0 +1,13 @@
+package max.iv.tz_convenient_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TzConvenientSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TzConvenientSoftwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/max/iv/tz_convenient_software/TzConvenientSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package max.iv.tz_convenient_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TzConvenientSoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
